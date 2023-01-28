46106d28ae09d56e23c7957dcf432016fe7bd87c
PreetAngadSingh <preetangad107@gmail.com>
2023-01-28 16:28:19 -0500

    removed static

--- src/PartTimeEmployee.java
+++ src/PartTimeEmployee.java
@@ -1,22 +1,23 @@
-public class PartTimeEmployee extends Employee{
+public class PartTimeEmployee extends Employee {
     int echelon;
     double hourlyRate;
     double hours;
-    static int[] echelonArr={15,20,25,30,40};
+    int[] echelonArr = { 15, 20, 25, 30, 40 };
 
-    PartTimeEmployee(String firstName, String lastName, int age, int id, int echelon, double hours){
+    PartTimeEmployee(String firstName, String lastName, int age, int id, int echelon, double hours) {
         super(firstName, lastName, age, id);
         this.echelon = echelon;
         this.hours = hours;
         hourlyRate = assignHourlyRate(echelon);
     }
 
-    double assignHourlyRate(int echelon){
-        if(echelon <6)
-        return echelonArr[echelon-1];
+    double assignHourlyRate(int echelon) {
+        if (echelon < 6)
+            return echelonArr[echelon - 1];
         else
-        return 0;
+            return 0;
     }
+
     public double monthlySalary() {
         return hourlyRate * hours;
     }
